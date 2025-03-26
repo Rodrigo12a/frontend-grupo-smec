7a0ed735696059e0a22c512573f3471df4c3cfcf
Rodrigo Osorio <rodrigo.osorio.rl@gmail.com>
2025-03-26 01:00:43 -0600

    nombre id suario

--- src/app/components/shared/cotizaciones/cotizaciones.component.ts
+++ src/app/components/shared/cotizaciones/cotizaciones.component.ts
@@ -1,19 +1,26 @@
+// cotizaciones.component.ts
 import { Component } from '@angular/core';
+import { forkJoin } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { CotizacionService } from '../../../services/cotizacion.service';
 import { Cotizacion } from '../../../interfaces/contizacion';
 import { CommonModule } from '@angular/common';
 import { TipoCotizacionPipe } from '../../../pipes/pipes';
+import { UserCrudService } from '../../../services/user-crud.service';
 
 @Component({
   selector: 'app-cotizaciones',
   imports: [CommonModule, TipoCotizacionPipe],
   templateUrl: './cotizaciones.component.html',
-  styleUrl: './cotizaciones.component.scss'
+  styleUrls: ['./cotizaciones.component.scss']
 })
 export class CotizacionesComponent {
-  listCotizacion: Cotizacion[] = []; // Array de Cotizacion
+  listCotizacion: Cotizacion[] = [];
 
-  constructor(private _cotizacionesService: CotizacionService) { }
+  constructor(
+    private _cotizacionesService: CotizacionService,
+    private _usuarioService: UserCrudService  // Verifica que el nombre y el tipo sean correctos
+  ) { }
 
   ngOnInit(): void {
     this.getListCotizacion();
@@ -21,9 +28,26 @@ export class CotizacionesComponent {
 
   getListCotizacion() {
     this._cotizacionesService.getCotizacion().subscribe({
-      next: (data: Cotizacion[]) => { // Asegura el tipo de respuesta
-        console.log('Datos recibidos:', data); // Revisa en la consola del navegador
-        this.listCotizacion = data;
+      next: (data: Cotizacion[]) => {
+        console.log('Datos recibidos de cotizaciones:', data);
+        const observables = data.map(cotizacion =>
+          this._usuarioService.getUsuario(cotizacion.id_usuario).pipe(
+            map((usuario) => {
+              // Ahora usuario es de tipo usuario, gracias a get<Usuario>()
+              cotizacion.usuario = usuario;
+              return cotizacion;
+            })
+          )
+        );
+        forkJoin(observables).subscribe({
+          next: (cotizacionesActualizadas: Cotizacion[]) => {
+            this.listCotizacion = cotizacionesActualizadas;
+            console.log('Cotizaciones con usuario:', cotizacionesActualizadas);
+          },
+          error: (err) => {
+            console.error("Error obteniendo datos de usuarios:", err);
+          }
+        });
       },
       error: (err) => {
         console.error("Error obteniendo cotizaciones:", err);
